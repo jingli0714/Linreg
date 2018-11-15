327a09e8c0e57d9ce41045fa16b0b9338f1f3e08
jingli0714 <lijing19890714@gmail.com>
2018-11-15 12:49:22 -0500

    1

--- R/linreg.R
+++ R/linreg.R
@@ -1,3 +1,30 @@
+#' Linear regression
+#'
+#' Runs an OLS regression not unlike \code{\link{lm}}
+#'
+#' @param x covariate matrix (p x n) with no intercept
+#' @param y response vector (1 x n)
+#'
+#'
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
+#'
+#' @examples
+#' data(mtcars)
+#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
+#' y <- mtcars[, "mpg"]
+#' linmodEst(y, X)
+#'
+#' @export
+#'
+
+# # If not already run when creating package with RStudio:
+# # git init
+# git add *
+# git commit -m "First commit"
+# # Replace cchoirat with your GitHub username
+# git remote add origin https://github.com/cchoirat/Linreg
+# git push -u origin master
+
 linmodEst <- function(x, y) {
   coef <- solve(t(x) %*% x) %*% t(x) %*% y
   ## degrees of freedom and standard deviation of residuals
